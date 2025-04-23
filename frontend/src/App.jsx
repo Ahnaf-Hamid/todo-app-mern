5fc5ec79ec81ad7928b89b4b67f40234a628c86c
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-04-23 00:04:36 -0700

    tailwind install

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { Routes } from 'react-router-dom'
+import { Routes,Route } from 'react-router-dom'
 import Navbar from './Navbar'
+import Login from './pages/Login'
+import Todo from './pages/Todo'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={d}/>
+        <Route path='/' element={<Login />}/>
+        <Route path='/todo-app' element={<Todo />}/>
       </Routes>
     </div>
   )
