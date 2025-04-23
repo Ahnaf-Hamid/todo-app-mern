52d0187c851ecf1481e0cf724e32a0b8565175c8
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-04-23 00:22:14 -0700

    today work done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="border w-96 relative left-40 top-40">
+      <form className="flex flex-col gap-2 p-2">
+        <input type="text" className="border " />
+        <input type="email" className="border " />
+        <input type="password" className="border " />
+        <button type="submit" className="border">Login</button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
