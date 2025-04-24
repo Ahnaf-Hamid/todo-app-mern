870ef0c32d208a1e51860e0a0727f130eaacddbc
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-04-24 11:36:04 -0700

    commited day 3

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import "dotenv/config";
+
+
+const app = express()
+const port = 4000
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send('Ápi working')
+})
+
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    app.listen(port,()=>{
+        console.log(`db connected and port running on localhost:${port}`);
+    })
+}).catch((err)=>{
+    console.log(err);
+})
+
