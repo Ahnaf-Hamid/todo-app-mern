494f1a0853ae92310ea9979ce463bbc942ac8003
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-04-25 11:12:15 -0700

    Day 4 todo model complete

---
+++ backend/model/todoModel.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+  todo: { type: String, required: true },
+  date: { type: Date, default: Date.now() },
+});
+
+const todoModel = mongoose.model.todo || mongoose.model('todo',todoSchema)
+
+export default todoModel
